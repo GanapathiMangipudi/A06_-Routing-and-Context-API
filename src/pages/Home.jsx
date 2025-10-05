28e05c4cf3981352c2897f43ca55f3b68319c2f3
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-10-05 18:28:36 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Post Management App 📑</h1>
+      <p>
+        Navigate to <strong>Posts</strong> to fetch and view posts.
+      </p>
+    </div>
+  );
+}
